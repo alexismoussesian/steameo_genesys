e01bad4f509036fe1bc27e45fee5037ad325fdce
alexismoussesian <alexis.moussesian@gmail.com>
2024-12-10 15:47:25 +0100

    ajout d'un wrapup code de test

--- wrapupcode.tf
+++ wrapupcode.tf
@@ -2,3 +2,7 @@ resource "genesyscloud_routing_wrapupcode" "TEC_SUPPRIMER_ENREGISTREMENT" {
     name = "TEC_SUPPRIMER_ENREGISTREMENT"
     division_id = "${data.genesyscloud_auth_division.Home.id}"
 }
+resource "genesyscloud_routing_wrapupcode" "TEC_TEST2" {
+    name = "TEC_TEST2"
+    division_id = "${data.genesyscloud_auth_division.Home.id}"
+}
