cd9e12ca5d89bd5356e8d52a5ccd1859c6f11a33
alexismoussesian <alexis.moussesian@gmail.com>
2024-12-10 15:49:35 +0100

    maj du wrapup code

--- wrapupcode.tf
+++ wrapupcode.tf
@@ -2,7 +2,3 @@ resource "genesyscloud_routing_wrapupcode" "TEC_SUPPRIMER_ENREGISTREMENT" {
     name = "TEC_SUPPRIMER_ENREGISTREMENT"
     division_id = "${data.genesyscloud_auth_division.Home.id}"
 }
-resource "genesyscloud_routing_wrapupcode" "TEC_TEST2" {
-    name = "TEC_TEST2"
-    division_id = "${data.genesyscloud_auth_division.Home.id}"
-}
